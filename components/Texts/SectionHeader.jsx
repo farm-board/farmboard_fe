0daed6b61d6d90c2cf925051bc90c52f3590492b
Dawson Timmons <DawsonTimmons@gmail.com>
2024-04-02 12:46:48 -0600

    add edit dashboard and profile styling

---
+++ components/Texts/SectionHeader.jsx
@@ -0,0 +1,33 @@
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import StyledText from "../Texts/StyledText";
+
+const SectionHeader = ({ children, option, style, onPress }) => {
+  return (
+    <View style={[styles.sectionHead, style]}>
+      <StyledText bold style={styles.headText}>
+        {children}
+      </StyledText>
+      <TouchableOpacity onPress={onPress}>
+        <StyledText small style={styles.optionText}>
+          {option}
+        </StyledText>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionHead: {
+    flexDirection: "row",
+    marginBottom: 20,
+  },
+  headText: {
+    flex: 1,
+    color: "#ECE3CE",
+  },
+  optionText: {
+    color: "#ECE3CE" + "aa",
+  },
+});
+
+export default SectionHeader;
--- components/farm/FarmProfile.jsx
+++ components/farm/FarmProfile.jsx
@@ -1,16 +1,20 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React, { useContext, useState, useEffect } from 'react'
 import { UserContext } from '../../contexts/UserContext'
 import axios from 'axios'
 import Animated, { FadeInUp, FadeInDown } from 'react-native-reanimated'
 import Avatar from '../Profile/Avatar'
 import StyledText from '../Texts/StyledText'
-
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { colors } from '../../config/theme'
+import { useNavigation } from '@react-navigation/native'
 
 export default function FarmProfile() {
   const {currentUser} = useContext(UserContext);
   const [farm, setFarm] = useState({});
   const [profilePhoto, setProfilePhoto] = useState(null);
+
+  const navigation = useNavigation();
 
   useEffect(() => {
     Promise.all([
@@ -31,27 +35,161 @@ export default function FarmProfile() {
     return <Text>Loading...</Text>;
   }
 
+  const onEditButtonPress = () => {
+    navigation.push('Profile Edit');
+  }
+
   return (
-    <View className="items-center">
-      <Animated.View entering={FadeInDown.delay(1000).duration(1000).springify()} className="mb-3">
-        <Avatar uri={profilePhoto} />
-      </Animated.View>
-      <Animated.Text entering={FadeInUp.duration(400).springify()} className="text-white tracking-wider pb-5">
-          <StyledText big>
-            {`${farm.name}`}
-          </StyledText>
-      </Animated.Text>
-      <Animated.Text entering={FadeInUp.duration(600).springify()} className="text-white tracking-wider pb-5">
-        <StyledText big>
-          {`${farm.city}, ${farm.state} ${farm.zip_code}`}
+    <View style={styles.container}>
+      <View style={styles.topSectionContainer}>
+          <TouchableOpacity style={styles.editButton} onPress={onEditButtonPress}>
+            <MaterialCommunityIcons
+              name="pencil-outline"
+              size={25}
+              color={colors.highlight}
+            />
+          </TouchableOpacity>
+        <View style={styles.leftContent}>
+          <View style={[styles.avatarContainer, styles.marginBottom3]}>
+            <Avatar uri={profilePhoto} />
+          </View>
+        </View>
+        <View style={styles.rightContent}>
+          <Text style={styles.farmName}>
+            <StyledText big bold tanColor >
+              {`${farm.name}`}
+            </StyledText>
+          </Text>
+          <Text style={styles.farmAddress}>
+            <StyledText tanColor >
+              {`${farm.city}, ${farm.state}`}
+            </StyledText>
+          </Text>
+        </View>
+      </View>
+      <View style={styles.farmBioContainer}>
+        <StyledText big tanColor style={styles.farmBioTitle}>
+          About
         </StyledText>
-      </Animated.Text>
-      <Animated.Text entering={FadeInUp.duration(800).springify()} className="text-white font-bold tracking-wider text-1xl pb-10">
-        <StyledText small>
+        <StyledText small tanColor style={styles.farmBioText}>
           {`${farm.bio}`}
         </StyledText>
-      </Animated.Text>
+      </View>
+      <View style={styles.accommodationsContainer}>
+        <Text style={styles.accommodationTitle}>
+          <StyledText big bold tanColor style={styles.accommodationTitle}>
+            Accommodations
+          </StyledText>
+        </Text>
+        <Text style={styles.accommodationListItem}>
+          <StyledText small tanColor>
+            Offers Housing: Yes
+          </StyledText>
+        </Text>
+        <Text style={styles.accommodationListItem}>
+          <StyledText small tanColor>
+            Offers Meals: Yes
+          </StyledText>
+        </Text>
+        <Text style={styles.accommodationListItem}>
+          <StyledText small tanColor>
+            Offers Transporation: Yes
+          </StyledText>
+        </Text>
+      </View>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  avatarContainer: {
+  },
+  topSectionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 25,
+    backgroundColor: '#4F6F52',
+    maxWidth: '100%',
+    shadowRadius: 20,
+    shadowColor: 'black',
+    shadowOpacity: 0.4,
+  },
+  leftContent: {
+    marginTop: 20,
+  },
+  rightContent: {
+    flex: 1,
+    marginTop: 20,
+  },
+  farmName: {
+    textAlign: 'right',
+    marginEnd: 20,
+    letterSpacing: 1,
+    paddingBottom: 5,
+  },
+  farmAddress: {
+    textAlign: 'right',
+    marginEnd: 20,
+    letterSpacing: 1,
+    marginBottom: 20,
+  },
+  farmBioText: {
+    letterSpacing: 1,
+    fontSize: 13,
+    marginVertical: 5,
+  },
+  farmBioTitle: {
+    letterSpacing: 1,
+    fontSize: 15,
+  },
+  farmBioContainer: {
+    letterSpacing: 1,
+    paddingHorizontal: 25,
+    paddingVertical: 10,
+    backgroundColor: '#4F6F52',
+    marginVertical: 10,
+    shadowRadius: 20,
+    shadowColor: 'black',
+    shadowOpacity: 0.3,
+    minWidth: '100%',
+  },
+  accommodationsContainer: {
+    letterSpacing: 1,
+    paddingHorizontal: 25,
+    paddingVertical: 10,
+    backgroundColor: '#4F6F52',
+    minWidth: '100%',
+    shadowRadius: 20,
+    shadowColor: 'black',
+    shadowOpacity: 0.4,
+  },
+  accommodationTitle: {
+    letterSpacing: 1,
+    fontSize: 15,
+    paddingBottom: 5,
+  },
+  accommodationListItem: {
+    letterSpacing: 1,
+    marginVertical: 5,
+    fontSize: 13,
+  },
+  paddingBottom5: {
+    paddingBottom: 5,
+  },
+  paddingBottom10: {
+    paddingBottom: 10,
+  },
+  editButton: {
+    backgroundColor: "#739072",
+    borderRadius: 24,
+    padding: 8,
+    position: "absolute",
+    right: 15,
+    top: 15,
+  },
+});
 
